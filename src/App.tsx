20b3f18819979037e018f259de21e642b11bd591
soyoon.seol <soyoon.seol@emotion.co.kr>
2024-07-10 11:20:27 +0900

    Router파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import Router from "./Router";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { GlobalStyle } from "./style/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Store from "./pages/Store";
+import Stores from "./pages/Stores";
 
 const queryClient = new QueryClient({
   /* options */
@@ -11,7 +13,16 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
-      <Router />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:storeId">
+            <Store />
+          </Route>
+          <Route path="/">
+            <Stores />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
